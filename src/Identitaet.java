88e1ba693ffa9e5a114d8d2a936dbdfc3d60a0db
Nico Brauer <nicobr.286@gmail.com>
2024-12-18 11:25:17 +0100

    Oberklasse :hammer:
    Erstellung der Oberklasse begonnen

---
+++ src/Identitaet.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.awt.*;
+
+public class Identitaet {
+    private int x;
+    private int y;
+    private int breite;
+    private int hoehe;
+    private Image img;
+    private int id;
+
+    public Identitaet(int x, int y, int breite, int hoehe, Image img, int id) {
+        this.x = x;
+        this.y = y;
+        this.breite = breite;
+        this.hoehe = hoehe;
+        this.img = img;
+        this.id = id;
+    }
+}

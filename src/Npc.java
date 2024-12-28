2c8f337590b644f3a1984514c77ee731b335ef60
Nico Brauer <nicobr.286@gmail.com>
2024-12-28 15:22:32 +0100

    NPC Bewegung 🐛
    Npc sucht sich nun eine neue Richtung, falls die vorher gewählte Richtung nicht begehbar sein sollte.

--- src/Npc.java
+++ src/Npc.java
@@ -10,6 +10,7 @@ public class Npc extends Identitaet {
     private Image figur;
     private Random rnd;
     private int richtung;
+    private boolean richtungGefunden;
 
     public Npc(int x, int y, int breite, int hoehe, int id) {
         super(x, y, breite, hoehe, id);
@@ -23,33 +24,40 @@ public class Npc extends Identitaet {
     }
 
     public void bewegen(int[][] map, ZeichenFlaeche14 zf) {
-        richtung = rnd.nextInt(4);
-        switch (richtung) {
-            case 0:
-                if (map[y-1][x] == 0) {
-                    zf.verschieben(id, 0, -hoehe);
-                    y--;
-                }
-                break;
-            case 1:
-                if (map[y+1][x] == 0) {
-                    zf.verschieben(id, 0, hoehe);
-                    y++;
-                }
-                break;
-            case 2:
-                if (map[y][x-1] == 0) {
-                    zf.verschieben(id, -breite, 0);
-                    x--;
-                }
-                break;
-            case 3:
-                if (map[y][x+1] == 0) {
-                    zf.verschieben(id, breite, 0);
-                    x++;
-                }
-                break;
-            default:
+        richtungGefunden = false;
+        while (!richtungGefunden) {
+            richtung = rnd.nextInt(4);
+            switch (richtung) {
+                case 0:
+                    if (map[y-1][x] == 0) {
+                        zf.verschieben(id, 0, -hoehe);
+                        y--;
+                        richtungGefunden = true;
+                    }
+                    break;
+                case 1:
+                    if (map[y+1][x] == 0) {
+                        zf.verschieben(id, 0, hoehe);
+                        y++;
+                        richtungGefunden = true;
+                    }
+                    break;
+                case 2:
+                    if (map[y][x-1] == 0) {
+                        zf.verschieben(id, -breite, 0);
+                        x--;
+                        richtungGefunden = true;
+                    }
+                    break;
+                case 3:
+                    if (map[y][x+1] == 0) {
+                        zf.verschieben(id, breite, 0);
+                        x++;
+                        richtungGefunden = true;
+                    }
+                    break;
+                default:
+            }
         }
     }
 

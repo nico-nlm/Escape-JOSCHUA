850ff7ab57276b8b54600cab5702a01e8b65a906
Nico Brauer <nicobr.286@gmail.com>
2025-01-21 21:59:38 +0100

    Grafiken Klasse :art:
    Klasse erstellt. Implementierung folgt

---
+++ src/Grafiken.java
@@ -0,0 +1,106 @@
+package src;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Grafiken {
+    //Figuren
+    private String spieler;
+    private String spielerKrug;
+    private String joschua;
+    private Image spielerImg;
+    private Image spielerKrugImg;
+    private Image joschuaImg;
+
+    //Items
+    private String krug;
+    private Image krugImg;
+
+    //Maps
+    private String hauptraum;
+    private String druckplattenraum;
+    private String schablone;
+    private String signal;
+    private Image hauptraumImg;
+    private Image druckplattenraumImg;
+    private Image schabloneImg;
+    private Image signalImg;
+
+    Grafiken() {
+        //Figuren
+        spieler = "src/img/figuren/spieler.png";
+        spielerKrug = "src/img/figuren/spieler-krug.png";
+        joschua =  "src/img/figuren/joschua.jpg";
+
+        //Items
+        krug = "src/img/items/krug.PNG";
+
+        //Maps
+        hauptraum = "src/img/maps/hauptraum.png";
+        druckplattenraum = "src/img/maps/druckplattenraum.png";
+        schablone = "src/img/maps/schablone.png";
+        signal = "src/img/maps/signal.PNG";
+
+        try {
+            //Figuren
+            spielerImg = ImageIO.read(new File(spieler));
+            spielerKrugImg = ImageIO.read(new File(spielerKrug));
+            joschuaImg = ImageIO.read(new File(joschua));
+
+            //Items
+            krugImg = ImageIO.read(new File(krug));
+
+            //Maps
+            hauptraumImg = ImageIO.read(new File(hauptraum));
+            druckplattenraumImg = ImageIO.read(new File(druckplattenraum));
+            schabloneImg = ImageIO.read(new File(schablone));
+            signalImg = ImageIO.read(new File(signal));
+        } catch (IOException e) {
+            System.err.println("Eine Figur konnte nicht geladen werden");
+        }
+
+    }
+
+    // Getter-Methoden für die Images
+
+    // Figuren
+    public Image getSpielerImg() {
+        return spielerImg;
+    }
+
+    public Image getSpielerKrugImg() {
+        return spielerKrugImg;
+    }
+
+    public Image getJoschuaImg() {
+        return joschuaImg;
+    }
+
+    // Items
+    public Image getKrugImg() {
+        return krugImg;
+    }
+
+    // Maps
+    public Image getHauptraumImg() {
+        return hauptraumImg;
+    }
+
+    public Image getDruckplattenraumImg() {
+        return druckplattenraumImg;
+    }
+
+    public Image getSchabloneImg() {
+        return schabloneImg;
+    }
+
+    public Image getSignalImg() {
+        return signalImg;
+    }
+
+
+
+}

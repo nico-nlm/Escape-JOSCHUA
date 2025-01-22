ac7b1d6bf7c5a96c0c7b98c64f8cfab9ab148c54
Nico Brauer <nicobr.286@gmail.com>
2025-01-22 09:58:44 +0100

    Schablone 🚧
    Schablone des Darkrooms hat nun eigene Klasse

---
+++ src/Schablone.java
@@ -0,0 +1,9 @@
+package src;
+
+import java.awt.*;
+
+public class Schablone extends Identitaet{
+    Schablone(int x, int y, int breite, int hoehe, Image grafik, int id) {
+        super(x, y, breite, hoehe, grafik, id);
+    }
+}
